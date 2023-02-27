eb88b8654d9deaaeb693ac6f93059bf5480a2c0f
Leonardo Dos Santos Dias <dleonardo@adhosting.internal>
2023-02-27 08:33:09 -0300

    feat/ Criando ação para fechar o modal

--- script.js
+++ script.js
@@ -3,30 +3,37 @@ const select        = (el) => document.querySelector(el);
 const selectAll     = (el) => document.querySelectorAll(el); 
 
 //* Inserindo os dados das pizzas na tela
-pizzaJson.map((el, index) => {
-    let pizzaItem = select('.models .pizza-item').cloneNode(true);
+pizzaJson.map((el, index) => {    
+
+let pizzaItem = select('.models .pizza-item').cloneNode(true);
     pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src     = el.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$: ${el.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML  = el.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML  = el.description;
     pizzaItem.querySelector('a').addEventListener('click', (e) => {     
-        e.preventDefault();    
+    e.preventDefault();    
 
-        //* Inserindo as informações das pizzas no Modal
-        let key = e.target.closest('.pizza-item').getAttribute('data-key');
-        select('.pizzaBig img').src                 = pizzaJson[key].img;
-        select('.pizzaInfo h1').innerHTML           = pizzaJson[key].name;
-        select('.pizzaInfo--desc').innerHTML        = pizzaJson[key].description;
-        select('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price;
+    //* Inserindo as informações das pizzas no Modal
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    select('.pizzaBig img').src                 = pizzaJson[key].img;
+    select('.pizzaInfo h1').innerHTML           = pizzaJson[key].name;
+    select('.pizzaInfo--desc').innerHTML        = pizzaJson[key].description;
+    select('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price;
 
-        //* Criando efeito no modal
-        select('.pizzaWindowArea').style.opacity     = 0;
-        select('.pizzaWindowArea').style.display     ='flex';
-        setTimeout(() => {
-            select('.pizzaWindowArea').style.opacity = 1;
-        }, 200);        
-    });
-
-    select('.pizza-area').append(pizzaItem);
+    //* Criando efeito no modal
+    select('.pizzaWindowArea').style.opacity     = 0;
+    select('.pizzaWindowArea').style.display     = 'flex';
+    setTimeout(() => {
+        select('.pizzaWindowArea').style.opacity = 1;
+    }, 200);        
 });
+    //* Ação de fechar modal
+    select('.pizzaInfo--cancelButton').addEventListener('click', () => {
+        select('.pizzaWindowArea').style.display = 'none';
+    });      
+
+    select('.pizza-area').append(pizzaItem);    
+});
+
+
