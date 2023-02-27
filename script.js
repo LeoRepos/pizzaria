bb0292b3f66a8e23fa998f3f4fa118db626a3801
Leonardo Dos Santos Dias <dleonardo@adhosting.internal>
2023-02-27 16:40:46 -0300

    feat/ Adicionando todas as informacoes no modal e evento de fechar modalMobile

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let modalQt = 1;
+
 //* criando seletores auxiliares para pegar elementos do HTML de forma simplificada  
 const select        = (el) => document.querySelector(el); 
 const selectAll     = (el) => document.querySelectorAll(el); 
+
 
 //* Inserindo os dados das pizzas na tela
 pizzaJson.map((el, index) => {    
@@ -16,10 +19,20 @@ let pizzaItem = select('.models .pizza-item').cloneNode(true);
 
     //* Inserindo as informações das pizzas no Modal
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
+    modalQt = 1;
     select('.pizzaBig img').src                 = pizzaJson[key].img;
     select('.pizzaInfo h1').innerHTML           = pizzaJson[key].name;
     select('.pizzaInfo--desc').innerHTML        = pizzaJson[key].description;
-    select('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price;
+    select('.pizzaInfo--actualPrice').innerHTML = `R$: ${pizzaJson[key].price.toFixed(2)}`;
+    select('.pizzaInfo--size.selected').classList.remove('selected');
+    selectAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+        if (sizeIndex == 2) {
+            size.classList.add('selected');
+        } 
+        size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+    });
+
+    select('.pizzaInfo--qt').innerHTML = modalQt;
 
     //* Criando efeito no modal
     select('.pizzaWindowArea').style.opacity     = 0;
@@ -31,7 +44,12 @@ let pizzaItem = select('.models .pizza-item').cloneNode(true);
     //* Ação de fechar modal
     select('.pizzaInfo--cancelButton').addEventListener('click', () => {
         select('.pizzaWindowArea').style.display = 'none';
-    });      
+    });  
+    
+    //* Ação de fechar modalMobile
+    select('.pizzaInfo--cancelMobileButton').addEventListener('click', () => {
+        select('.pizzaWindowArea').style.display = 'none';
+    });   
 
     select('.pizza-area').append(pizzaItem);    
 });
